cc3136ddfa4106994007132aedcd7741c895e60f
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-08-04 11:45:27 -0300

    Desafio05

--- Desafios/Desafio5/Desafio05.js
+++ Desafios/Desafio5/Desafio05.js
@@ -2,15 +2,14 @@ const codeForcesIO = require("@ip-algorithmics/codeforces-io");
 const gets = codeForcesIO.readline;
 const print = codeForcesIO.print;
 
-let times = gets();
-let n = parseInt(times);
-let partidas = 0;
-let totalPartidas = 0;
-while (n > 1) {
-  n--;
-  if (n % 2 == 0) {
-    print(n / 2);
-  } else if (n % 2 !== 0) {
-    print((n - 1) / 2);
+let x = gets();
+
+function soma(x) {
+  while (x >= 1) {
+    if (x % 2 == 0) x = (x - 1) / 2;
+    else x = (x - 1) / 2;
   }
+  return x
 }
+
+print(soma(x));
